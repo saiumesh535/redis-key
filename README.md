dc7482c656be63ddf38a4f2c3f625f50b4c3464b
saiumesh535 <d.sai535@gmail.com>
2020-01-27 15:58:15 +0530

    added custom URL

--- README.md
+++ README.md
@@ -9,5 +9,16 @@ This tool helps us to extract value for given key, as redis windows, doesn't sup
 key=redis_key ./redis-key.exe
 ```
 
+## by default it uses redis url as following
+```cmd
+redis://localhost:6379
+```
+
+## using custom url
+```cmd
+key=redis_key URL=redis://localhost:6379 ./redis-key.exe
+```
+
+
 ## Download executables from below
 [download](https://github.com/saiumesh535/redis-key/releases)
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ use redis::{Commands};
 use std::env;
 use std::process;
 
-fn fetch_key(key: &String) -> redis::RedisResult<String> {
-    let redis_url: &str = "redis://localhost:6379";
-    let client = redis::Client::open(redis_url)?;
+fn fetch_key(key: &String, url: &str) -> redis::RedisResult<String> {
+    let client = redis::Client::open(url)?;
     let mut con = client.get_connection()?;
     con.get(key)
 }
 
 fn main() {
     let key = env::var("key").expect("key cannot be empty");
-    match fetch_key(&key) {
+    let redis_url = env::var("URL").unwrap_or(String::from("redis://localhost:6379"));
+    match fetch_key(&key, redis_url.as_str()) {
         Ok(value) => println!("{}", value),
         Err(err) => {
             println!("{}", err.to_string());
