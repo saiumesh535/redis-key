5c0d655cb582e1b61ee5f90e7da0203909db6cb3
saiumesh535 <d.sai535@gmail.com>
2020-01-27 15:46:47 +0530

    first implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # redis-key
 Extracts value for given key from redis, written in Rust 🦀
+
+This tool helps us to extract value for given key, as redis windows, doesn't support extracting value for given key
+
+## Commands to run
+
+```cmd
+key=redis_key ./redis-key.exe
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use redis::{Commands};
+use std::env;
+use std::process;
+
+fn fetch_key(key: &String) -> redis::RedisResult<String> {
+    let redis_url: &str = "redis://localhost:6379";
+    let client = redis::Client::open(redis_url)?;
+    let mut con = client.get_connection()?;
+    con.get(key)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let key = env::var("key").expect("key cannot be empty");
+    match fetch_key(&key) {
+        Ok(value) => println!("{}", value),
+        Err(err) => {
+            println!("{}", err.to_string());
+            process::exit(1);
+        }
+    };
 }
